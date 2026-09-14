23e09f7c1e23504dacddab17f486ebde34eb6b6e
nik16i01ta07-web <nik16i01ta07@gmail.com>
2026-09-14 09:12:44 +0000

    Employee console application

---
+++ EmployeeApp/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Employee
+{
+    public string FullName;
+    public double Salary;
+
+    public void Show()
+    {
+        Console.WriteLine("ФИО сотрудника: " + FullName);
+        Console.WriteLine("Месячная зарплата: " + Salary + " руб.");
+    }
+
+    public double CalculateAnnualSalary()
+    {
+        return Salary * 12;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Employee employee = new Employee();
+
+        employee.FullName = "Иванов";
+        employee.Salary = 63500;
+
+        employee.Show();
+
+        double annualSalary = employee.CalculateAnnualSalary();
+
+        Console.WriteLine("Годовая зарплата: " + annualSalary + " руб.");
+    }
+}
